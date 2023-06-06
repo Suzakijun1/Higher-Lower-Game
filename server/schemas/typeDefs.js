42ff4e1c4c69f4888593b0a882c0e9cc7ec218ed
David Peguero <david.peguero.cancel@gmail.com>
2023-06-06 10:13:32 -0400

    Created some basic typeDefs

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,23 @@
+const typeDefs = gql`
+  type User {
+    _id: ID
+    email: String
+    password: String
+  }
+
+  type Auth {
+    token: ID!
+    user: User
+  }
+
+  type Query {
+    users: [User]
+    user(username: String!): User
+    me: User
+  }
+
+  type Mutation {
+    addUser(email: String!, password: String!) : Auth
+
+  }
+`

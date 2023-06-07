6c48f22136f8f1feba21e1864461b7af90fe10de
David Peguero <david.peguero.cancel@gmail.com>
2023-06-07 12:08:21 -0400

    Added way to add to teams

--- client/src/pages/DraftGame.js
+++ client/src/pages/DraftGame.js
@@ -7,12 +7,19 @@ import {HERO_IMG} from "../utils/queries"
 export default function Home() {
   const [teamOne, setTeamOne] = useState([]);
   const [teamTwo, setTeamTwo] = useState([]);
+  const [seenIds, setSeenIds] = useState([])
   const [isDrafting, setIsDrafting] = useState(true);
   const [heroOneId, setHeroOneId] = useState(Math.floor(Math.random() * 730 + 1));
-  const [heroTwoId, setHeroTwoId] = useState(Math.floor(Math.random() * 730 + 1));
-  
-  
-
+  const [heroTwoId, setHeroTwoId] = useState(() => {
+    let newId;
+    while(true){
+      newId = Math.floor(Math.random() * 730 + 1)
+      if(newId !== heroOneId){
+        break
+      }
+    }
+    return newId;
+  });
   
 
   return (
@@ -24,6 +31,13 @@ export default function Home() {
       <DraftBody 
       heroOneId={heroOneId}
       heroTwoId={heroTwoId}
+      setHeroOneId={setHeroOneId}
+      setHeroTwoId={setHeroTwoId}
+      teamOne={teamOne}
+      teamTwo={teamTwo}
+      setTeamOne={setTeamOne}
+      setTeamTwo={setTeamTwo}
+      setIsDrafting={setIsDrafting}
       />
     </div>
   );

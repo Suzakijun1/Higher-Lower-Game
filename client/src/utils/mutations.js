3c1feac2d24a37466c018d66e1d98705539d8825
David Peguero <david.peguero.cancel@gmail.com>
2023-06-06 21:51:37 -0400

    Sign up page now adds users with mutation

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,11 +1,14 @@
 import { gql } from "@apollo/client";
 
 export const ADD_USER = gql`
-  mutation Mutation($email: String!, $username: String!, $password: String!) {
+mutation Mutation($email: String!, $username: String!, $password: String!) {
   addUser(email: $email, username: $username, password: $password) {
-    username
-    password
-    email
+    user {
+      username
+      password
+      email
+    }
+    token
   }
 }
 `;

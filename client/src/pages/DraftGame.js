c60d61ffac917113fdc22b07e3f31ad2b1255807
David Peguero <david.peguero.cancel@gmail.com>
2023-06-07 10:54:40 -0400

    Added query to retrieve hero images by id

--- client/src/pages/DraftGame.js
+++ client/src/pages/DraftGame.js
@@ -1,8 +1,18 @@
-import React from "react";
+import {useEffect, useState} from "react";
 import DraftBody from "../components/draft-game-body";
 import NavBar from "../components/navbar";
 
 export default function Home() {
+  const [teamOne, setTeamOne] = useState([]);
+  const [teamTwo, setTeamTwo] = useState([]);
+  const [isDrafting, setIsDrafting] = useState(true);
+
+
+  useEffect(() => {
+    
+  }, [])
+  
+
   return (
     <div>
       <h1 className="text-3xl font-bold text-center mt-8">

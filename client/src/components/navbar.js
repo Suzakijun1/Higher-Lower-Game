f8165526e2e5b8c353fbb3793d34fb78885047b8
jalvarez322 <alvarez.julio322@gmail.com>
2023-06-07 19:52:14 -0400

    updated navbar

--- client/src/components/navbar.js
+++ client/src/components/navbar.js
@@ -1,13 +1,12 @@
 import React, { useState, Fragment } from 'react';
 import { Disclosure, Menu, Transition } from '@headlessui/react';
-import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/react/24/outline';
+import { Bars3Icon, BellIcon, QuestionMarkCircleIcon, XMarkIcon } from '@heroicons/react/24/outline';
 import Modal from "./modal";
+
 
 const navigation = [
   { name: 'Home', href: '/', current: true },
-  { name: 'Team', href: '#', current: false },
-  { name: 'Projects', href: '#', current: false },
-  { name: 'Calendar', href: '#', current: false },
+  
 ];
 
 // Sample data for the modal
@@ -83,10 +82,10 @@ export default function NavBar({isModalOpen, setIsModalOpen}) {
               <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
                 <button
                   type="button"
-                  className="rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
+                  className="rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none"
                 >
-                  <span className="sr-only">View notifications</span>
-                  <BellIcon className="h-6 w-6" aria-hidden="true" />
+                  <span className="sr-only">How to play</span>
+                  <QuestionMarkCircleIcon className="h-6 w-6" aria-hidden="true" />
                 </button>
 
                 {/* Profile dropdown */}
@@ -96,7 +95,7 @@ export default function NavBar({isModalOpen, setIsModalOpen}) {
                       <span className="sr-only">Open user menu</span>
                       <img
                         className="h-8 w-8 rounded-full"
-                        src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                        src="https://cdn1.iconfinder.com/data/icons/avengers-1/512/avangers_icon003-512.png"
                         alt=""
                       />
                     </Menu.Button>
@@ -129,7 +128,7 @@ export default function NavBar({isModalOpen, setIsModalOpen}) {
                             href="#"
                             className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}
                           >
-                            Settings
+                            Sign in
                           </a>
                         )}
                       </Menu.Item>
@@ -188,3 +187,5 @@ export default function NavBar({isModalOpen, setIsModalOpen}) {
     </Disclosure>
   );
 }
+
+

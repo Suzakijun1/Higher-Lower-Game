1819d63a6f5e6d72a9a4c62f65315a2041ef6803
robles1999 <30624599+robles1999@users.noreply.github.com>
2023-06-08 17:12:14 -0400

    Added bg image to higherLower.

--- client/src/pages/HigherLower.js
+++ client/src/pages/HigherLower.js
@@ -1,9 +1,15 @@
 import HigherLowerBody from "../components/HigherLowerBody";
+import bgImage from "../images/battlegrounds-3.jpeg";
 
 export default function HigherLower() {
   return (
-    <div>
-      <h1 className="text-3xl font-bold text-center mt-8">
+    <div
+      className="bg-cover bg-center"
+      style={{
+        backgroundImage: `url(${bgImage})`,
+      }}
+    >
+      <h1 className="text-3xl font-bold text-center pt-10">
         Welcome to the Game!
       </h1>
       <HigherLowerBody />

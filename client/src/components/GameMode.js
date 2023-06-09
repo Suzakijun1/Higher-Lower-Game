bca6c9323462e38ac0246cb9b450ac32b92aa00a
robles1999 <30624599+robles1999@users.noreply.github.com>
2023-06-09 14:01:11 -0400

    Added sample sound.

--- client/src/components/GameMode.js
+++ client/src/components/GameMode.js
@@ -1,7 +1,11 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import soundFx from "../sounds/080953_semi-auto-pistol-39750.wav";
 
 const GameModesComponent = () => {
+  const playSound = () => {
+    new Audio(soundFx).play();
+  };
   return (
     <div className="flex flex-col items-center mt-20 h-screen">
       <div className="p-6 bg-gray-800 bg-opacity-60 rounded-lg border border-gray-400 w-80 shadow-xl shadow-blue-500/70 ">
@@ -10,6 +14,7 @@ const GameModesComponent = () => {
           <Link
             to="/draftgame"
             className="flex space-x-10 bg-blue-500 text-center text-white font-bold py-2 px-4 rounded items-center"
+            onClick={playSound}
           >
             <img
               className="w-12 h-12 bg-gray-200 rounded-full"
@@ -22,6 +27,7 @@ const GameModesComponent = () => {
           <Link
             to="/higherlower"
             className="flex space-x-10 bg-green-500 text-center text-white font-bold py-2 px-4 mb-5 rounded"
+            onClick={playSound}
           >
             <span className="ml-1 pt-3">Higher/Lower</span>
             <img

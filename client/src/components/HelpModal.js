f7f9cac747267c119fa405d4cc6746504311a030
trentstanley92 <trentstanley92@gmail.com>
2023-06-09 11:48:04 -0400

    styled help modal and added game descriptions

--- client/src/components/HelpModal.js
+++ client/src/components/HelpModal.js
@@ -5,7 +5,7 @@ const Modal = ({ isOpen, onClose, wins, losses, totalGames,highestStreak, avgStr
 
   return (
     <div id="defaultModal" className="flex justify-center items-center overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
-      <div className='bg-red-300 rounded-lg shadow-xl p-4 w-96 border-size 30px'>
+      <div className='bg-gray-800 rounded-lg shadow-xl p-4 w-96 border-size 30px'>
         <div className="flex justify-end">
           <button
             data-modal-hide="defaultModal"
@@ -15,11 +15,15 @@ const Modal = ({ isOpen, onClose, wins, losses, totalGames,highestStreak, avgStr
             Close
           </button>
         </div>
-        <div className="mt-4 textAlign text-center">
+        <div className="mt-2 textAlign text-center">
             
-        <h1 className="text-xl" style={{ fontFamily: 'Luckiest Guy, Cursive',  fontSize: '50px' }}>HOW TO PLAY</h1>
+        <h1 className="text-xl" style={{ fontFamily: 'Luckiest Guy, Cursive',  fontSize: '34px' }}>HOW TO PLAY</h1>
         <br></br>
-          <p className="text-sm" style={{ fontFamily: 'VT323, Monospace', fontSize: '30px'}}>test how does this look like a real game test how does this look like a real game test how does this look like a real game test how does this look like a real game test how does this look like a real game</p>
+          <p className="text-sm" style={{ fontFamily: 'VT323, Monospace', fontSize: '20px',color:''}}>Choose between the Higher Lower or Draft game to start. </p><br></br>
+          <p className="text-sm" style={{ fontFamily: 'VT323, Monospace', fontSize: '30px'}}>Draft:  </p>
+          <p className="text-sm" style={{ fontFamily: 'VT323, Monospace', fontSize: '20px'}}>You will be prompted with two characters to select from in order to build a team of 5 each with their own individual stats. Once your team is drafted you will go head to head with another team and choose which attack you would like to use and with which character in order to reduce their health to zero!  </p><br></br>
+          <p className="text-sm" style={{ fontFamily: 'VT323, Monospace', fontSize: '30px'}}>Higher Lower:  </p>
+          <p className="text-sm" style={{ fontFamily: 'VT323, Monospace', fontSize: '20px'}}>You will be given 2 heroes and are prompted to choose whether the given heroes listed attribute is higher or lower than the other heroes same attribute.  </p>
         </div>
       </div>
     </div>

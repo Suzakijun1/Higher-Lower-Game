c7ffbd3a8cb3f17554a12e07d72c2c19dd69ecfb
robles1999 <30624599+robles1999@users.noreply.github.com>
2023-06-09 15:07:12 -0400

    Theme sound track added to the public index.html file.

--- client/src/components/GameMode.js
+++ client/src/components/GameMode.js
@@ -2,11 +2,13 @@ import React from "react";
 import { Link } from "react-router-dom";
 import soundFx from "../sounds/080953_semi-auto-pistol-39750.wav";
 
+
 const GameModesComponent = () => {
   const playSound = () => {
     new Audio(soundFx).play();
   };
-  return (
+
+return (
     <div className="flex flex-col items-center mt-20 h-screen">
       <div className="p-6 bg-gray-800 bg-opacity-60 rounded-lg border border-gray-400 w-80 shadow-xl shadow-blue-500/70 ">
         <h1 className="text-2xl font-bold mb-4 text-center">Game Modes</h1>

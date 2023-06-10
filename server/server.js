93b63d2cd5843a254db41e6d0e50c58cd93c142f
David Peguero <david.peguero.cancel@gmail.com>
2023-06-10 14:53:30 -0400

    Added path import

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const db = require("./config/connection");
 const { typeDefs, resolvers } = require('./schemas');
 const { authMiddleware } = require('./utils/auth');
 const { ApolloServer } = require("apollo-server-express");
+const path = require('path');
 
 
 const PORT = process.env.PORT || 3001;

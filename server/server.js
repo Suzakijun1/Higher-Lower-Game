300661a4cd308403e53e6c8231ecbf51e238d9b5
David Peguero <david.peguero.cancel@gmail.com>
2023-06-10 15:03:59 -0400

    Added server route to index.html

--- server/server.js
+++ server/server.js
@@ -22,6 +22,9 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, './client/build')));
 }
 
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, './index.html'));
+});
 
 const startApolloServer = async () => {
   await server.start();
